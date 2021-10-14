34346bb939aed4d5ed2570cfddfdf7216fd48ab1
Manu Abuya <manuabuya69@gmail.com>
2021-10-15 00:44:03 +0300

    Initial Commit

--- app/src/main/java/com/example/vehicare/MainActivity.java
+++ app/src/main/java/com/example/vehicare/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.vehicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
+    // After completion of 2000 ms, the next activity will get started.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //This method is used so that the splash screen activity can cover the entire screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //This binds the MainActivity.class with the activity_main layout
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //Intent is used to switch from one activity to another.
+                Intent i = new Intent(MainActivity.this, StartupActivity.class);
+
+                //invoke the Startup Activity
+                startActivity(i);
+
+                //The current activity will get finished
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
---
+++ app/src/main/java/com/example/vehicare/VehicleRegActivity.java
@@ -0,0 +1,14 @@
+package com.example.vehicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_reg);
+    }
+}
