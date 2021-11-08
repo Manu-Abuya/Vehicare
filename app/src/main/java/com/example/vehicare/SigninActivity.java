ced3db9001e7e2a86e61256006bcfdde329f34f2
Manu-Abuya <manuabuya69@gmail.com>
2021-11-08 08:58:19 +0300

    Initial Commit

--- app/src/main/java/com/example/vehicare/SigninActivity.java
+++ app/src/main/java/com/example/vehicare/SigninActivity.java
@@ -2,13 +2,39 @@ package com.example.vehicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class SigninActivity extends AppCompatActivity {
+
+    ImageButton ImageButton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        ImageButton = findViewById(R.id.img_back_Signin);
+        textView = findViewById(R.id.textView_SignUp);
+
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SigninActivity.this, StartupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
